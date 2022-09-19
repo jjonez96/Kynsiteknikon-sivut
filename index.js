ede6b906f2ec33fde12d0a43d4cc5729ee7512f7
Joona Luukkonen <joona114@hotmail.fi>
2022-09-19 23:16:56 +0300

    new color scheme
    scripts are now in seperate js file

---
+++ index.js
@@ -0,0 +1,26 @@
+function responsiveNav() {
+  let nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "topnav";
+  }
+}
+let mybutton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
